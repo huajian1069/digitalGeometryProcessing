4a8ef35e8d050c74b69dfd48be7b482ae4c43573
Huajian Chiu <huajian.qiu@epfl.ch>
2019-12-09 17:22:33 +0100

    finish angle constraints

---
+++ ShapeUp/build/externals/tetgen/CMakeFiles/tetgen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetgen.dir/predicates.cpp.o"
+  "CMakeFiles/tetgen.dir/tetgen.cpp.o"
+  "libtetgen.1.0.dylib"
+  "libtetgen.dylib"
+  "libtetgen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
